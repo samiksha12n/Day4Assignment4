21e428134a6516a1588590f20e56d8e0fd97337c
samiksha12n <samikshaunesarkar@gmail.com>
2023-11-09 15:43:51 +0530

    Displaying array

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Part4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 1, 2, 6, 2, 18 };
+
+            int i = 0;
+            int sum = 0;
+            float average = 0.0F;
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            average = (float)sum / arr.Length;
+
+            Console.WriteLine("Average of Array elements: " + average);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
